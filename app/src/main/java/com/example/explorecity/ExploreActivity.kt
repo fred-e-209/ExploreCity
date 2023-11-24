a1c00b460c8040852a2da0dbdeb2f481659cb6e2
David Liu <david_liu@tamu.edu>
2023-11-24 17:04:35 -0600

    Added markers to maps

--- app/src/main/java/com/example/explorecity/ExploreActivity.kt
+++ app/src/main/java/com/example/explorecity/ExploreActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,14 +26,11 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -43,12 +39,13 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.explorecity.ui.theme.DarkBlue
 import androidx.navigation.NavController
-import com.example.explorecity.api.classes.event.emptySingleEventResponse
+import com.example.explorecity.api.classes.event.EventDetailBody
 import com.example.explorecity.api.models.ApiViewModel
 import com.example.explorecity.api.models.EventStorage
 import com.example.explorecity.api.models.UserInformation
+import com.example.explorecity.ui.theme.DarkBlue
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -57,7 +54,7 @@ import com.google.maps.android.compose.MarkerInfoWindow
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -68,6 +65,14 @@ fun ExploreActivity(navController: NavController, viewModel: ApiViewModel) {
 
     val darkBlue = Color(0xFF003366)
     val white = Color.White
+
+    val userEvents by viewModel.userEvents.observeAsState(emptyList())
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchUserEvents()
+        delay(1000)
+        viewModel.updateEventsToday(userEvents)
+    }
 
     Scaffold(
         topBar = {
@@ -111,10 +116,11 @@ fun ExploreActivity(navController: NavController, viewModel: ApiViewModel) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .padding(bottom = 100.dp)
         ) {
             when (viewMode.value) {
-                "map" -> GoogleMapsView(navController, viewMode)
-                "list" -> EventsListView(navController, viewModel)
+                "map" -> GoogleMapsView(navController, userEvents)
+                "list" -> EventsListView(navController, userEvents)
                 "details" -> DetailsActivity(navController, viewModel)
             }
         }
@@ -123,7 +129,7 @@ fun ExploreActivity(navController: NavController, viewModel: ApiViewModel) {
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun GoogleMapsView(navController: NavController, viewMode: MutableState<String>) {
+fun GoogleMapsView(navController: NavController, events: List<EventDetailBody>) {
     val userLocation = UserInformation.instance.getUserLocation()
     var currentUserLat by remember { mutableDoubleStateOf(0.0) }
     var currentUserLon by remember { mutableDoubleStateOf(0.0) }
@@ -139,36 +145,43 @@ fun GoogleMapsView(navController: NavController, viewMode: MutableState<String>)
         Marker(
             state = rememberMarkerState(position = LatLng(currentUserLat, currentUserLon)),
             draggable = true,
-            title = "Cstat",
-            snippet = "Time: 8:00 pm"
+            title = "Your Location",
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)
         )
-        MarkerInfoWindow(
-            state = MarkerState(position = LatLng(30.610657, -96.340695)),
-            onInfoWindowClick = {
-                navController.navigate("details")
-            }
-
-        )
-        { marker ->
-            Box(
-                modifier = Modifier
-                    .padding(16.dp) // Adjust padding for spacing
-                    .background(Color.White, shape = RoundedCornerShape(8.dp)) // Adjust corner radius
-            ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
+        for (event in events){
+            MarkerInfoWindow(
+                state = MarkerState(position = LatLng(event.coords.lat, event.coords.lon)),
+                onInfoWindowClick = {
+                    EventStorage.instance.setEventID(event.id)
+                    navController.navigate("details")
+                },
+                title = event.displayname,
+                snippet = formatDate(event.start)
+            )
+            { marker ->
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp) // Adjust padding for spacing
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(8.dp)
+                        ) // Adjust corner radius
                 ) {
-                    Text(
-                        text = marker.title ?: "Aggie Football",
-                        color = Color.Black,
-                        style = TextStyle(fontWeight = FontWeight.Bold)
-                    )
-                    Spacer(modifier = Modifier.height(8.dp)) // Add more vertical space
-                    Text(
-                        text = marker.snippet ?: "Time: 8:00PM",
-                        color = Color.Black,
-                        style = TextStyle(fontWeight = FontWeight.Normal)
-                    )
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(
+                            text = marker.title ?: "Unknown",
+                            color = Color.Black,
+                            style = TextStyle(fontWeight = FontWeight.Bold)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp)) // Add more vertical space
+                        Text(
+                            text = marker.snippet ?: "Time: Unknown",
+                            color = Color.Black,
+                            style = TextStyle(fontWeight = FontWeight.Normal)
+                        )
+                    }
                 }
             }
         }
@@ -178,14 +191,8 @@ fun GoogleMapsView(navController: NavController, viewMode: MutableState<String>)
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
-fun EventsListView(navController: NavController, viewModel: ApiViewModel) {
+fun EventsListView(navController: NavController, events: List<EventDetailBody>) {
     val eventStorageInstance = EventStorage.instance
-
-    val events by viewModel.userEvents.observeAsState(emptyList())
-
-    LaunchedEffect(Unit) {
-        viewModel.fetchUserEvents()
-    }
     Column(modifier = Modifier.fillMaxSize()) {
         Row(
             modifier = Modifier
